00f1a1bf3fdedbfa9dd5306bba50f988f4f9ed41
izzetbasli <77061424+izzetbasli@users.noreply.github.com>
2021-11-18 17:42:12 +0300

    Add files via upload

--- meyvesınıflandırma.py
+++ meyvesınıflandırma.py
@@ -13,22 +13,15 @@ import cv2
 from torch.utils.data import DataLoader
 from sklearn.metrics import accuracy_score
 
-# In[99]:
 
-
-# Loading data
 transforms_train = T.Compose([T.ToTensor(), T.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
 image_data_train = ImageFolder("fruits-360-original-size/Training", transform=transforms_train)
 image_data_test = ImageFolder("fruits-360-original-size/Validation", transform=transforms_train)
 
-# Shuffling data and then collecting all the labels.
+
 random.shuffle(image_data_train.samples)
 random.shuffle(image_data_test.samples)
 
-# In[100]:
-
-
-# Total classes
 classes_idx = image_data_train.class_to_idx
 classes = len(image_data_train.classes)
 len_train_data = len(image_data_train)
@@ -36,7 +29,7 @@ len_test_data = len(image_data_test)
 
 
 def get_labels():
-    labels_train = []  # All the labels
+    labels_train = []
     labels_test = []
     for i in image_data_train.imgs:
         labels_train.append(i[1])
@@ -48,32 +41,16 @@ def get_labels():
 
 
 labels_train, labels_test = get_labels()
-
-# In[101]:
-
-
 train_loader = DataLoader(dataset=image_data_train, batch_size=100)
 test_loader = DataLoader(dataset=image_data_test, batch_size=100)
 
-# In[102]:
-
-
 print(iter(train_loader).next()[0].shape)
 record = iter(train_loader).next()[0]
-
-
-# - We can see that the image is (batch_size, channel, image_height, image_width)
-
-# In[103]:
-
-
-# Flatten Layer
 class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
 
 
-# Main Model
 class Model:
     def build_model(self):
         model = nn.Sequential(nn.Conv2d(3, 64, kernel_size=5, stride=1), nn.ReLU(), nn.MaxPool2d(2),
@@ -83,31 +60,19 @@ class Model:
         return model
 
 
-# In[104]:
-
-
-# Building
 model = Model()
 model = model.build_model()
-
-# In[109]:
 
 
 print(model)
 print(labels_train[0])
 print(image_data_train.samples[0][1])
 
-# In[107]:
 
 
 optimizer = torch.optim.Adagrad(model.parameters(), lr=0.01)
 criterion = nn.CrossEntropyLoss()
 
-
-# In[124]:
-
-
-# Train the model
 def train(epochs):
     model.train()
     losses = []
@@ -117,20 +82,18 @@ def train(epochs):
         for feature, label in train_loader:
             x = Variable(feature, requires_grad=False).float()
             y = Variable(label, requires_grad=False).long()
-            optimizer.zero_grad()  # Zeroing the grads
-            y_pred = model(x)  # Calculating prediction
+            optimizer.zero_grad()
+            y_pred = model(x)
             correct = y_pred.max(1)[1].eq(y).sum()
             print("no. of correct items classified: ", correct.item())
-            loss = criterion(y_pred, y)  # Calculating loss (log_softmax already included)
+            loss = criterion(y_pred, y)
             print("loss: ", loss.item())
             current_loss += loss.item()
-            loss.backward()  # Gradient cal
-            optimizer.step()  # Changing weights
-        losses.append(current_loss)  # Only storing loss after every epoch
+            loss.backward()
+            optimizer.step()
+        losses.append(current_loss)
     return losses
 
-
-# Test the model
 def test():
     model.eval()
     with torch.no_grad():
@@ -141,18 +104,6 @@ def test():
             print("loss: ", loss.item())
 
 
-# In[125]:
-
-
-# Training
 train(100)
-
-# In[130]:
-
-
 list(model.named_parameters())
-
-# In[132]:
-
-
 len(labels_train)
